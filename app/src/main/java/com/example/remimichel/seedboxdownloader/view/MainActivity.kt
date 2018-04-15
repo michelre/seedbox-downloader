10669c88c9b76ec4ee586efabb63576d5e5f6bd5
remimichel <remi.michel38@gmail.com>
2018-04-15 23:14:11 +0200

    feat(): allow bottom navigation with fragments

--- app/src/main/java/com/example/remimichel/seedboxdownloader/view/MainActivity.kt
+++ app/src/main/java/com/example/remimichel/seedboxdownloader/view/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.remimichel.seedboxdownloader.view
 
 import android.content.Context
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -14,32 +12,18 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import com.example.remimichel.seedboxdownloader.R
 import com.example.remimichel.seedboxdownloader.data.remote.File
-import com.example.remimichel.seedboxdownloader.presenter.*
+import com.example.remimichel.seedboxdownloader.presenter.FtpListView
+import com.example.remimichel.seedboxdownloader.presenter.initFtpContent
+import com.example.remimichel.seedboxdownloader.presenter.onBackButtonClick
 
-class MainActivity : AppCompatActivity(), FtpListView {
+class MainActivity : AppCompatActivity(),
+    FtpListView,
+    FilesFragment.OnFragmentInteractionListener,
+    TorrentsFragment.OnFragmentInteractionListener,
+    SettingsFragment.OnFragmentInteractionListener {
 
   private var mTextMessage: TextView? = null
   private lateinit var adapter: FileFTPAdapter
-
-  /*private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-    when (item.itemId) {
-      R.id.navigation_home -> {
-        mTextMessage!!.setText(R.string.title_files)
-        return@OnNavigationItemSelectedListener true
-      }
-      R.id.navigation_dashboard -> {
-        mTextMessage!!.setText(R.string.title_torrents)
-        return@OnNavigationItemSelectedListener true
-      }
-      R.id.navigation_notifications -> {
-        //mTextMessage!!.setText(R.string.title_settings)
-        val i = Intent(this, SettingsActivity::class.java);
-        startActivity(i)
-        return@OnNavigationItemSelectedListener true
-      }
-    }
-    false
-  }*/
 
   //https://android.jlelse.eu/ultimate-guide-to-bottom-navigation-on-android-75e4efb8105f
 
@@ -54,6 +38,15 @@ class MainActivity : AppCompatActivity(), FtpListView {
         AHBottomNavigationItem(getString(R.string.title_torrents), R.drawable.ic_home_black_24dp),
         AHBottomNavigationItem(getString(R.string.title_settings), R.drawable.ic_home_black_24dp)
     ))
+    addFragment(FilesFragment())
+    navigation.setOnTabSelectedListener { position, _ ->
+      when (position) {
+        0 -> addFragment(FilesFragment())
+        1 -> addFragment(TorrentsFragment())
+        2 -> addFragment(SettingsFragment())
+        else -> true
+      }
+    }
     setupFTPList()
     initFtpContent(this, this.getCredentials("server1"))
   }
@@ -73,6 +66,8 @@ class MainActivity : AppCompatActivity(), FtpListView {
     Log.e("APPP", "", error)
   }
 
+  override fun onFragmentInteraction(uri: Uri) {}
+
   fun getCredentials(server: String): Map<String, String> {
     val sharedPreferences = this.getSharedPreferences("settings", Context.MODE_PRIVATE)
     return mapOf(
@@ -88,6 +83,13 @@ class MainActivity : AppCompatActivity(), FtpListView {
     adapter = FileFTPAdapter(listOf(), { onFtpListItemClick(this, it) })
     recyclerView.layoutManager = LinearLayoutManager(this)
     recyclerView.adapter = adapter*/
+  }
+
+  fun addFragment(f: Fragment): Boolean {
+    val fragmentTransaction = supportFragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.frame, f)
+    fragmentTransaction.commit()
+    return true
   }
 
 }
